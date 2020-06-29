013f4a7bf8a23b96157db4f84652622b42416328
David MacNeil <david.macneil@actual-experience.com>
2020-06-29 17:25:02 +0100

    Remove failed attempt at resources

--- helm-template.sh
+++ helm-template.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-helm template -f base/values.yaml stable/prometheus-operator | sed 's/RELEASE-NAME/monitoring/g' > base/prometheus-operator.yaml
+helm template -f values.yaml monitoring stable/prometheus-operator > base/prometheus-operator.yaml
